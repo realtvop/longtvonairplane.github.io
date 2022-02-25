a1197e865fc79184cf3f74f7c250d52a17bd96ea
飞机上的长电视 <79362411+longtvonairplane@users.noreply.github.com>
2022-02-25 13:34:06 +0800

    Update main.js

--- utils/main.js
+++ utils/main.js
@@ -4,9 +4,9 @@
 	const d = "lch\x7ah3473";
 	const w = `作者：<a style="text-decoration:underline"target="_blank"href="//space.bilibili.com/274753872">${d}</a>`;
 	//if (!location.search) location.search = Date.now();
-	if (typeof _i == "undefined" || _i.length != 4) return;
-	if (!(jct && jct[1] == "ok" || document.referrer)) return location.href = "/401.html";
-	document.cookie = `jct=ok;path=/;max-age=${2e6}`
+	#if (typeof _i == "undefined" || _i.length != 4) return;
+	#if (!(jct && jct[1] == "ok" || document.referrer)) return location.href = "/401.html";
+	#document.cookie = `jct=ok;path=/;max-age=${2e6}`
 	document.title = `${_i[0]} - ${d}制作`;
 	for (const i of document.querySelectorAll(".title")) i.innerHTML = `${_i[0]}&nbsp;v${_i[1].join('.')}`;
 	for (const i of document.querySelectorAll(".info")) i.innerHTML = `${w}&nbsp;(${cnymd(_i[2])}制作)<br><br>最后更新于${cnymd(_i[3])}`;
