3524929370f423a99b441e0db26fe3006a2823be
飞机上的长电视 <79362411+longtvonairplane@users.noreply.github.com>
2022-02-25 13:35:30 +0800

    Update main.js

--- utils/main.js
+++ utils/main.js
@@ -6,7 +6,7 @@
 	//if (!location.search) location.search = Date.now();
 	#if (typeof _i == "undefined" || _i.length != 4) return;
 	#if (!(jct && jct[1] == "ok" || document.referrer)) return location.href = "/401.html";
-	#document.cookie = `jct=ok;path=/;max-age=${2e6}`
+	document.cookie = `jct=ok;path=/;max-age=${2e6}`
 	document.title = `${_i[0]} - ${d}制作`;
 	for (const i of document.querySelectorAll(".title")) i.innerHTML = `${_i[0]}&nbsp;v${_i[1].join('.')}`;
 	for (const i of document.querySelectorAll(".info")) i.innerHTML = `${w}&nbsp;(${cnymd(_i[2])}制作)<br><br>最后更新于${cnymd(_i[3])}`;
