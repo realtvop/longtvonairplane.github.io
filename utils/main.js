6184326e19c02094c509122421883c7f20d0938b
飞机上的长电视 <79362411+longtvonairplane@users.noreply.github.com>
2022-02-25 13:39:39 +0800

    Update main.js

--- utils/main.js
+++ utils/main.js
@@ -3,8 +3,8 @@
 	//const jct = document.cookie.match(/jct=(.+?)(;|$)/);
 	const d = "lch\x7ah3473";
 	const w = `作者：<a style="text-decoration:underline"target="_blank"href="//space.bilibili.com/274753872">${d}</a>`;
-	//if (!location.search) location.search = Date.now();
-	//if (typeof _i == "undefined" || _i.length != 4) return;
+	if (!location.search) location.search = Date.now();
+	if (typeof _i == "undefined" || _i.length != 4) return;
 	//if (!(jct && jct[1] == "ok" || document.referrer)) return location.href = "/401.html";
 	document.cookie = `jct=ok;path=/;max-age=${2e6}`
 	document.title = `${_i[0]} - ${d}制作`;
