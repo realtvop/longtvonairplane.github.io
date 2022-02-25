ca6073cef5019621608b482edaa2796475f10bba
飞机上的长电视 <79362411+longtvonairplane@users.noreply.github.com>
2022-02-25 13:38:52 +0800

    Update main.js

--- utils/main.js
+++ utils/main.js
@@ -1,11 +1,11 @@
 "use strict";
 (function() {
-	#const jct = document.cookie.match(/jct=(.+?)(;|$)/);
+	//const jct = document.cookie.match(/jct=(.+?)(;|$)/);
 	const d = "lch\x7ah3473";
 	const w = `作者：<a style="text-decoration:underline"target="_blank"href="//space.bilibili.com/274753872">${d}</a>`;
 	//if (!location.search) location.search = Date.now();
-	if (typeof _i == "undefined" || _i.length != 4) return;
-	if (!(jct && jct[1] == "ok" || document.referrer)) return location.href = "/401.html";
+	//if (typeof _i == "undefined" || _i.length != 4) return;
+	//if (!(jct && jct[1] == "ok" || document.referrer)) return location.href = "/401.html";
 	document.cookie = `jct=ok;path=/;max-age=${2e6}`
 	document.title = `${_i[0]} - ${d}制作`;
 	for (const i of document.querySelectorAll(".title")) i.innerHTML = `${_i[0]}&nbsp;v${_i[1].join('.')}`;
